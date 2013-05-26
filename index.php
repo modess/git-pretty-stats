d2d0f9bafbc6b0a33db180b629fee17fb1f20574
Niklas Modess <niklas.modess@gmail.com>
2013-05-26 12:25:13 +0200

    Added error handling for config and repository

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 
 require_once __DIR__.'/vendor/autoload.php';
-$config = require_once __DIR__ . '/config.php';
+
+use Symfony\Component\HttpFoundation\Response;
 
 $app = new Silex\Application();
 
@@ -9,7 +10,33 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/views',
 ));
 
-$app['repository'] = new PrettyGit\GitRepository(__DIR__ . '/' . $config['repositoryPath']);
+$app->before(function() use ($app) {
+    if ($app['request']->getRequestUri() == '/error') {
+        return true;
+    }
+
+    $error = false;
+    $configFilePath = __DIR__ . '/config.php';
+    if (!file_exists($configFilePath)) {
+        return displayError('Config (config.php) file does not exists');
+    }
+
+    $config = require_once __DIR__ . '/config.php';
+    if (!isset($config['repositoryPath'])) {
+        return displayError('Config value for repositoryPath does not exist');
+    }
+
+    try {
+        $app['repository'] = new PrettyGit\GitRepository(__DIR__ . '/' . $config['repositoryPath']);
+    } catch (Exception $e) {
+        return displayError('The repository path does not contain a valid git repository');
+    }
+});
+
+function displayError($message) {
+    global $app;
+    return new Response($app['twig']->render('error.html', array('message' => $message)), 500);
+}
 
 $app->get('/', function() use($app) {
     return $app['twig']->render(
