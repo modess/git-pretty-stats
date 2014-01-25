5c1af17ee5cadc7f06c75f8a3fc33814f215aee1
Niklas Modess <niklas.modess@gmail.com>
2014-01-25 14:56:55 +0100

    Ignore repository factory service provider in code coverage

--- app/GitPrettyStats/Providers/RepositoryFactoryServiceProvider.php
+++ app/GitPrettyStats/Providers/RepositoryFactoryServiceProvider.php
@@ -4,6 +4,10 @@ namespace GitPrettyStats\Providers;
 use Illuminate\Support\ServiceProvider;
 use GitPrettyStats\RepositoryFactory;
 
+/**
+ * Service provider for repository factory
+ * @codeCoverageIgnore
+ */
 class RepositoryFactoryServiceProvider extends ServiceProvider
 {
     public function register ()
