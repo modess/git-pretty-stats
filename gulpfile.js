ead6f73635c0877b12a8e27de95c72fda0ade486
Niklas Modess <niklas@codingswag.com>
2015-04-05 08:17:06 +0200

    folders in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -16,33 +16,47 @@ var gulp = require('gulp'),
   jshintstylish = require('jshint-stylish'),
   angularFilesort = require('gulp-angular-filesort');
 
+var assetsFolder          = './app/assets/';
+var assetsJsFolder        = assetsFolder + 'scripts/';
+var assetsSassFolder      = assetsFolder + 'styles/';
+var assetsViewsFolder     = assetsFolder + 'views/';
+var assetsTemplatesFolder = assetsJsFolder + 'templates/';
+
+var distFolder    = './public/';
+var distJsFolder  = distFolder + 'scripts/';
+var distCssFolder = distFolder + 'styles/';
+
 gulp.task('styles', function() {
-  return sass('app/assets/styles/', { sourcemap: true })
+  return sass(assetsSassFolder, { sourcemap: true })
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
     .pipe(concat('main.css'))
     .pipe(gulp.dest('public/styles'));
 });
 
 gulp.task('scripts', function() {
-  return gulp.src('app/assets/scripts/**/*.js')
+  return gulp.src(assetsJsFolder + '**/*.js')
     .pipe(angularFilesort())
     .pipe(concat('app.js'))
     .pipe(gulp.dest('public/scripts'));
 });
 
 gulp.task('jshint', function() {
-  return gulp.src('./app/assets/scripts/**/*.js')
+  return gulp.src(assetsJsFolder + '**/*.js')
     .pipe(jshint('.jshintrc'))
     .pipe(jshint.reporter(jshintstylish));
 });
 
 gulp.task('clean', function () {
-  return gulp.src(['public/**/*', '!public/index.php', '!public/.htaccess'], {read: false})
-    .pipe(clean());
+  return gulp.src([
+    distFolder + '**/*',
+    '!' + distFolder + 'index.php',
+    '!' + distFolder + '.htaccess'
+  ], {read: false})
+  .pipe(clean());
 });
 
 gulp.task('angular-templates', function () {
-  gulp.src(['app/assets/views/**/*.html', 'app/assets/scripts/templates/**/*.html'])
+  gulp.src([assetsViewsFolder + '/**/*.html', assetsTemplatesFolder + '**/*.html'])
     .pipe(templateCache({
       module: 'gitPrettyStats.templates',
       standalone: true
@@ -54,21 +68,21 @@ gulp.task('vendor', function() {
   var styles = gulp.src(mainBowerFiles())
     .pipe(gulpFilter('*.css'))
     .pipe(concat('vendor.css'))
-    .pipe(gulp.dest('public/styles'));
+    .pipe(gulp.dest(distCssFolder));
 
   var scripts = gulp.src(mainBowerFiles())
     .pipe(gulpFilter('*.js'))
     .pipe(concat('vendor.js'))
-    .pipe(gulp.dest('public/scripts'));
+    .pipe(gulp.dest(distJsFolder));
 
   return merge(styles, scripts);
 });
 
 gulp.task('watch', ['default'], function () {
-  gulp.watch('app/assets/styles/**/*.scss', ['styles']);
-  gulp.watch('app/assets/scripts/**/*.js', ['scripts', 'jshint']);
-  gulp.watch('app/assets/views/**/*.html', ['angular-templates']);
-  gulp.watch('public/**/*').on('change', livereload.changed);
+  gulp.watch(assetsSassFolder + '**/*.scss', ['styles']);
+  gulp.watch(assetsJsFolder + '**/*.js', ['scripts', 'jshint']);
+  gulp.watch([assetsViewsFolder + '**/*.html', assetsTemplatesFolder + '**/*.html'], ['angular-templates']);
+  gulp.watch(distFolder + '**/*').on('change', livereload.changed);
 });
 
 gulp.task('default', ['clean'], function () {
